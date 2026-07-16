f28a19ce5a51f08316638870c78e711e320dcb3c
Paulo <pguevara135@hotmail.com>
2026-07-16 17:26:37 -0300

    Atualizacao Surface

--- Desafios/desafio030/teste.py
+++ Desafios/desafio030/teste.py
@@ -1,8 +1,18 @@
-import hashlib
+# import hashlib
 
-texto = 'Gafanhoto'
+# texto = 'Gafanhoto'
 
-cod = texto.encode('utf-8')
-hash = hashlib.sha256(cod).hexdigest()
+# cod = texto.encode('utf-8')
+# hash = hashlib.sha256(cod).hexdigest()
 
-print(hash)
+# print(hash)
+
+class Dog:  
+    def __init__(self, name):  
+        self.name = name
+
+    def bark(self):  
+        print(f"{self.name} says Woof!")  
+
+my_dog = Dog("Rex")
+print(my_dog.name)
