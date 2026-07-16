a40b5af3b8e4cb094e6ae60cb5ea1189bc8c961a
pguevara135 <pguevara135@hotmail.com>
2026-07-16 18:19:42 -0300

    Criado o retangulo.py

--- Desafios/desafio031/__main__.py
+++ Desafios/desafio031/__main__.py
@@ -1,7 +1,18 @@
 from retangulo import Retangulo
 
+
 def main():
-    pass
+
+    r = Retangulo(7, 4)
+    try:
+        r.base = 12
+        r.altura = 4
+        r.medidas = (8, 12)
+    except Exception as e:
+        print(f'Ocorreu um erro de tipo: {type(e).__name__}: {e}')
+
+    print(r.medidas)
+
 
 if __name__ == '__main__':
     main()
--- Desafios/desafio031/retangulo.py
+++ Desafios/desafio031/retangulo.py
@@ -1,2 +1,63 @@
 class Retangulo:
-    pass
+    def __init__(self, base = 1, altura = 1):
+        self._base = None
+        self._altura = None
+        self._area = None
+        
+        self.base = base
+        self.altura = altura
+        
+    @property
+    def base(self):
+        return self._base
+    
+    @base.setter
+    def base(self, valor):
+        if not isinstance(valor, int) and not isinstance(valor, float):
+            raise TypeError('O valor da base deve ser um numero')
+        if valor < 0:
+            raise ValueError('O valor da base deve ser maior que zero')
+        else:
+            self._base = valor
+            
+    
+    @property
+    def altura(self):
+        return self._altura
+    
+    @altura.setter
+    def altura(self, valor):
+        if not isinstance(valor, int) and not isinstance(valor, float):
+            raise TypeError('O valor da altura deve ser um numero')
+        if valor < 0:
+            raise ValueError('O valor da altura deve ser maior que zero')
+        else:
+            self._altura = valor
+    
+    @property
+    def area(self):
+        self._area = self._base * self._altura
+        return self._area
+    
+    @area.setter
+    def area(self):
+        raise PermissionError('Area nao pode ser alterada diretamente. Use base e altura para calcular a area.')
+    
+    @property
+    def medidas(self):
+        return f'Base = {self.base} \nAltura = {self.altura} \nArea = {self.area}'
+    
+    @medidas.setter
+    def medidas(self, valores:tuple):
+        if not isinstance(valores, tuple):
+            raise TypeError('As medidas devem ser indomradas dentro de uma tupla')
+        if len(valores) != 2:
+            raise SyntaxError('Informe uma tuplas com dois valores')
+        if isinstance(valores[0], int) or isinstance(valores[0], float):
+            self.base = valores[0]
+        else:
+            raise TypeError('O valor da base deve ser um numero')
+        if isinstance(valores[1], int) or isinstance(valores[1], float):
+            self.altura = valores[1]
+        else:
+            raise TypeError('O valor da altura deve ser um numero')
